b8abc62135b7c635e8f6c8d0f46741fe1108a080
JeeLee <18810996718@163.com>
2017-05-26 15:19:15 +0800

    简单开发架子搭建

---
+++ src/components/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <a href="javascript:;" class="weui-btn weui-btn_primary">页面主操作 Normal</a>
+    <a href="javascript:;" class="weui-btn weui-btn_primary weui-btn_loading"><i class="weui-loading"></i>页面主操作 Loading</a>
+    <a href="javascript:;" class="weui-btn weui-btn_warn">警告类操作 Normal</a>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App1'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
